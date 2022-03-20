a88fc7a62b836239b13ff1c9d7165d5e3c6fcbc3
OmarRa15 <omarhoms0@gmail.com>
2022-03-20 07:43:06 +0300

    initial heroku build

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from os import environ
 
 app = Flask(__name__)
+# app.config['SECRET_KEY'] = environ['SECRET_KEY']
+# app.config['SQLALCHEMY_DATABASE_URI'] = environ['DATABASE_URL'][0:8] + 'ql' + environ['DATABASE_URL'][8:]
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 
 
 @app.route('/')
